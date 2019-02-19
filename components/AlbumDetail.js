bb3c13ecf7122474017ffb0c66260939d5e8f916
paulbriar <pbriar@live.com>
2019-02-18 20:19:01 -0400

    Wired up AlbumDetail images & text

--- components/AlbumDetail.js
+++ components/AlbumDetail.js
@@ -1,15 +1,40 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
 import Card from './Card';
+import CardSection from './CardSection';
 
-const AlbumDetail = (props) => {
-  const { album } = props;
+const AlbumDetail = ({ album }) => {
+  // eslint-disable-next-line camelcase
+  const { thumbnail_image, title, artist } = album;
+  const { thumbnailStyle, headerContent } = styles;
 
   return (
     <Card>
-      <Text>{album.title}</Text>
+      <CardSection>
+        <View>
+          <Image
+            style={thumbnailStyle}
+            source={{ uri: thumbnail_image }}
+          />
+        </View>
+        <View style={headerContent}>
+          <Text>{title}</Text>
+          <Text>{artist}</Text>
+        </View>
+      </CardSection>
     </Card>
   );
+};
+
+const styles = {
+  headerContent: {
+    flexDirection: 'column',
+    justifyContent: 'space-around'
+  },
+  thumbnailStyle: {
+    width: 50,
+    height: 50,
+  }
 };
 
 export default AlbumDetail;
